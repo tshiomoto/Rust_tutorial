1094215e1e8b7b488e284d46893846a543e9e49d
Tatsunori Shiomoto <ryuusyou3174@gmail.com>
2020-11-25 23:24:48 +0900

    webの骨組みのみを作成

--- web_todo/src/main.rs
+++ web_todo/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, ResponseError};
+use thiserror::Error;
+
+// Errorを返すenumを定義
+#[derive(Error, Debug)]
+enum MyError {}
+
+// ResponseErrorをMyErrorに実装
+impl ResponseError for MyError {}
+
+#[get("/")]
+async fn index() -> Result<HttpResponse, MyError> {
+    let response_body = "Hello, Rust!";
+
+    // HttpResponse::Ok()を返す
+    Ok(HttpResponse::Ok().body(response_body))
+}
+
+#[actix_rt::main]
+async fn main() -> Result<(), actix_web::Error> {
+    // 鯖建て
+    HttpServer::new(move || App::new().service(index))
+        .bind("0.0.0.0:8080")?
+        .run()
+        .await?;
+    Ok(())
 }
