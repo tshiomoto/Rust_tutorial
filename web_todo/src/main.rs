88f4b9664144a100e8eef1381b454a144a3b4088
Tatsunori Shiomoto <ryuusyou3174@gmail.com>
2020-12-02 22:50:33 +0900

    [Update] rustでhtmlを表示できるようにする

--- web_todo/src/main.rs
+++ web_todo/src/main.rs
@@ -1,24 +1,47 @@
 use actix_web::{get, App, HttpResponse, HttpServer, ResponseError};
+use askama::Template;
 use thiserror::Error;
 
-// Errorを返すenumを定義
-#[derive(Error, Debug)]
-enum MyError {}
+struct TodoEntry {
+    id: i32,
+    text: String,
+}
 
-// ResponseErrorをMyErrorに実装
+#[derive(Template)]
+#[template(path="index.html")]
+struct IndexTemplate {
+    entries: Vec<TodoEntry>,
+}
+
+#[derive(Error, Debug)]
+enum MyError {
+    #[error("Failed to render HTML")]
+    AskamaError(#[from] askama::Error),
+}
+
 impl ResponseError for MyError {}
 
 #[get("/")]
 async fn index() -> Result<HttpResponse, MyError> {
-    let response_body = "Hello, Rust!";
+    let mut entries = Vec::new();
+    entries.push(TodoEntry {
+        id: 1,
+        text: "First Entries".to_string(),
+    });
+    entries.push(TodoEntry {
+        id: 2,
+        text: "Second Entries".to_string(),
+    });
 
-    // HttpResponse::Ok()を返す
-    Ok(HttpResponse::Ok().body(response_body))
+    let html = IndexTemplate{entries};
+    let response_body = html.render()?;
+    Ok(HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response_body))
 }
 
 #[actix_rt::main]
 async fn main() -> Result<(), actix_web::Error> {
-    // 鯖建て
     HttpServer::new(move || App::new().service(index))
         .bind("0.0.0.0:8080")?
         .run()
